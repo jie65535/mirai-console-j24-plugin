ed5ee331262a2e17b059fe0cbb3b8462f7824f7d
jie65535 <jie65535@qq.com>
2025-03-04 00:45:37 +0800

    Update version to v1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    val kotlinVersion = "1.7.0"
+    val kotlinVersion = "1.8.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.12.0"
+    id("net.mamoe.mirai-console") version "2.16.0"
 }
 
 group = "top.jie65535"
-version = "0.1.3"
+version = "1.0.0"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
